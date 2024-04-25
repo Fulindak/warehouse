468ccced04bad22d26a6ca5efc00c33f3702cda3
danila <danilfulin@yandex.ru>
2024-04-25 13:44:21 +0300

    feat: add JpaTest from criteria

--- src/test/java/danila/mediasoft/test/warehouse/repositories/ProductRepositoryTest.java
+++ src/test/java/danila/mediasoft/test/warehouse/repositories/ProductRepositoryTest.java
@@ -1,14 +1,23 @@
 package danila.mediasoft.test.warehouse.repositories;
 
 import danila.mediasoft.test.warehouse.motherObject.ProductBuilder;
+import danila.mediasoft.test.warehouse.services.search.ProductSpecification;
+import danila.mediasoft.test.warehouse.services.search.creteria.BigDecimalCriteria;
+import danila.mediasoft.test.warehouse.services.search.creteria.Criteria;
+import danila.mediasoft.test.warehouse.services.search.creteria.LocalDateCriteria;
+import danila.mediasoft.test.warehouse.services.search.creteria.LongCriteria;
+import danila.mediasoft.test.warehouse.services.search.enums.Operation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -18,36 +27,200 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class ProductRepositoryTest {
     @Autowired
-    ProductRepository productRepository;
+    ProductRepository productRepositoryUnderTest;
+    @InjectMocks
+    ProductSpecification productSpecificationUnderTest;
 
-    @Autowired
-    ProductTypeRepository productTypeRepository;
+    final String fieldQuantity = "quantity";
+    final String fieldCreateAt = "createAt";
+
+    final String fieldPrice = "price";
+
 
     private final Integer dbSize = 50;
 
     @BeforeEach
     void setUp() {
-        productRepository.deleteAll();
-        productTypeRepository.deleteAll();
-        productTypeRepository.saveAll(ProductBuilder.DEFAULT_PRODUCT_TYPES);
-        productRepository.saveAll(IntStream.range(0, dbSize)
+        productRepositoryUnderTest.deleteAll();
+        var products = IntStream.range(0, dbSize)
                 .mapToObj(
                         index ->
                                 ProductBuilder
                                         .aDefaultProduct()
-                                        .withPrice(ProductBuilder.DEFAULT_PRICE.add(BigDecimal.valueOf(index)))
+                                        .withPrice(BigDecimal.valueOf(index + 50))
                                         .withArticle("article_" + index)
                                         .withName("product_" + index)
                                         .withId(UUID.randomUUID())
                                         .withQuantity((long) index)
+                                        .withProductTypes(new ArrayList<>())
                                         .build())
-                        .toList()
-                );
+                .toList();
+        productRepositoryUnderTest.saveAll(products);
     }
 
     @Test
-    void shouldFindAll() {
-        var products = productRepository.findAll();
+    void givenProductEntity_whenFindAll_thenReturnExpectedSize() {
+        var products = productRepositoryUnderTest.findAll();
         assertThat(products.size()).isEqualTo(dbSize);
+    }
+
+    @Test
+    void givenLocalDateCriteria_whenFindAll_thenReturnExpectedLikeProductList() {
+        List<Criteria> criteria = List.of(
+                LocalDateCriteria.builder()
+                        .value(ProductBuilder.DEFAULT_CREATE_AT)
+                        .field(fieldCreateAt)
+                        .operation(Operation.LIKE)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(dbSize);
+    }
+
+    @Test
+    void givenLocalDateCriteria_whenFindAll_thenReturnExpectedEqualProductList() {
+        List<Criteria> criteria = List.of(
+                LocalDateCriteria.builder()
+                        .value(ProductBuilder.DEFAULT_CREATE_AT)
+                        .field(fieldCreateAt)
+                        .operation(Operation.EQUAL)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(dbSize);
+
+    }
+
+    @Test
+    void givenLocalDateCriteria_whenFindAll_thenReturnExpectedGreaterProductList() {
+        List<Criteria> criteria = List.of(
+                LocalDateCriteria.builder()
+                        .value(ProductBuilder.DEFAULT_CREATE_AT)
+                        .field(fieldCreateAt)
+                        .operation(Operation.GREATER_THAN_OR_EQ)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(dbSize);
+
+    }
+
+    @Test
+    void givenLocalDateCriteria_whenFindAll_thenReturnExpectedLessProductList() {
+        List<Criteria> criteria = List.of(
+                LocalDateCriteria.builder()
+                        .value(ProductBuilder.DEFAULT_CREATE_AT)
+                        .field(fieldCreateAt)
+                        .operation(Operation.LESS_THAN_OR_EQ)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(dbSize);
+
+    }
+
+    @Test
+    void givenLongCriteria_whenFindAll_thenReturnExpectedLikeProductList() {
+        List<Criteria> criteria = List.of(
+                LongCriteria.builder()
+                        .value(1L)
+                        .field(fieldQuantity)
+                        .operation(Operation.LIKE)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(1);
+    }
+
+    @Test
+    void givenLongCriteria_whenFindAll_thenReturnExpectedEqualProductList() {
+        List<Criteria> criteria = List.of(
+                LongCriteria.builder()
+                        .value(25L)
+                        .field(fieldQuantity)
+                        .operation(Operation.EQUAL)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    void givenLongCriteria_whenFindAll_thenReturnExpectedGreaterProductList() {
+        List<Criteria> criteria = List.of(
+                LongCriteria.builder()
+                        .value(25L)
+                        .field(fieldQuantity)
+                        .operation(Operation.GREATER_THAN_OR_EQ)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(25);
+
+    }
+
+    @Test
+    void givenLongCriteria_whenFindAll_thenReturnExpectedLessProductList() {
+        List<Criteria> criteria = List.of(
+                LongCriteria.builder()
+                        .value(ProductBuilder.DEFAULT_QUANTITY)
+                        .field(fieldQuantity)
+                        .operation(Operation.LESS_THAN_OR_EQ)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(50);
+
+    }
+
+    @Test
+    void givenMultipleCriteria_whenFindAll_thenReturnExpectedProductList() {
+        List<Criteria> criteria = List.of(
+                BigDecimalCriteria.builder()
+                        .value(BigDecimal.valueOf(75))
+                        .field(fieldPrice)
+                        .operation(Operation.LESS_THAN_OR_EQ)
+                        .build(),
+                BigDecimalCriteria.builder()
+                        .value(BigDecimal.valueOf(60))
+                        .field(fieldPrice)
+                        .operation(Operation.GREATER_THAN_OR_EQ)
+                        .build(),
+                LocalDateCriteria.builder()
+                        .value(ProductBuilder.DEFAULT_CREATE_AT)
+                        .field(fieldCreateAt)
+                        .operation(Operation.LESS_THAN_OR_EQ)
+                        .build()
+        );
+        var products = productRepositoryUnderTest.findAll(
+                productSpecificationUnderTest.findProductByCriteria(criteria)
+        );
+
+        assertThat(products.size()).isEqualTo(16);
     }
 }
