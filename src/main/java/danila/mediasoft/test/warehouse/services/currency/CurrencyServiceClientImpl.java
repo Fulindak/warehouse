f0d5288a849cc8f262d4a161eb949adf868b570b
danila <danilkuzmin2011@gmail.com>
2024-05-02 09:07:12 +0300

    refactor: refactor CurrencyServiceClientImpl

--- src/main/java/danila/mediasoft/test/warehouse/services/currency/CurrencyServiceClientImpl.java
+++ src/main/java/danila/mediasoft/test/warehouse/services/currency/CurrencyServiceClientImpl.java
@@ -2,41 +2,55 @@ package danila.mediasoft.test.warehouse.services.currency;
 
 import danila.mediasoft.test.warehouse.config.CurrencyProperties;
 import danila.mediasoft.test.warehouse.dto.currency.CurrencyRates;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
+import java.math.BigDecimal;
 import java.time.Duration;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 @CacheConfig(cacheNames = "currencyRates")
 @ConditionalOnMissingBean(name = "currencyServiceMock")
+@RequiredArgsConstructor
 public class CurrencyServiceClientImpl implements CurrencyServiceClient {
     private final WebClient webClient;
-    private final String methodGetCurrency;
-
-    public CurrencyServiceClientImpl(WebClient.Builder webClient,
-                                     CurrencyProperties properties) {
-        this.methodGetCurrency = properties.methods().getCurrency();
-        this.webClient = webClient.baseUrl(properties.host()).build();
-    }
+    private final CurrencyProperties properties;
 
     @Override
     @Cacheable(cacheNames = "currencyRates", key = "#root.methodName", unless = "#result == null")
     public CurrencyRates getCurrencyRates() {
-        return webClient.get()
-                .uri(methodGetCurrency)
+        return CurrencyRates.builder()
+                .rates(webClient
+                        .get()
+                        .uri(properties.methods().getCurrency())
+                        .retrieve()
+                        .bodyToMono(new ParameterizedTypeReference<Map<String, BigDecimal>>(){})
+                        .retryWhen(Retry.backoff(2, Duration.ofSeconds(1)))
+                        .onErrorResume(ex -> {
+                            log.error(ex.getMessage());
+                            return Mono.empty();
+                        }).block()).build();
+    }
+
+    public String test() {
+        return webClient
+                .get()
+                .uri(properties.methods().getCurrency())
                 .retrieve()
-                .bodyToMono(CurrencyRates.class)
+                .bodyToMono(String.class)
                 .retryWhen(Retry.backoff(2, Duration.ofSeconds(1)))
                 .onErrorResume(ex -> {
                     log.error(ex.getMessage());
