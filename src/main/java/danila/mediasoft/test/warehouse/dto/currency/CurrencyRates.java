c64b18b6fce229623a291183a0a29c6dcf83c7e3
danila <danilkuzmin2011@gmail.com>
2024-05-02 09:06:28 +0300

    refactor: refactor CurrencyServiceClientImpl

--- src/main/java/danila/mediasoft/test/warehouse/dto/currency/CurrencyRates.java
+++ src/main/java/danila/mediasoft/test/warehouse/dto/currency/CurrencyRates.java
@@ -1,26 +1,18 @@
 package danila.mediasoft.test.warehouse.dto.currency;
 
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import danila.mediasoft.test.warehouse.enums.CurrencyType;
 import danila.mediasoft.test.warehouse.exceptions.IllegalCurrencyTypeException;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-@Getter
 @Builder
 @Data
 public class CurrencyRates {
     private Map<String, BigDecimal> rates = new HashMap<>();
-
-    @JsonAnySetter
-    public void setRate(String currency, BigDecimal rate) {
-        rates.put(currency, rate);
-    }
 
     public BigDecimal getRateByCurrencyType(CurrencyType currencyType) {
         if (!rates.containsKey(currencyType.name())) {
