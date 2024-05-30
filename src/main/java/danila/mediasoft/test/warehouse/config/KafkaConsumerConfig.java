50864ef0efdbf0c424c9c6d241f1b26852af1eca
danila <danilkuzmin2011@gmail.com>
2024-05-30 15:01:22 +0300

    feat:refactor

--- src/main/java/danila/mediasoft/test/warehouse/config/KafkaConsumerConfig.java
+++ src/main/java/danila/mediasoft/test/warehouse/config/KafkaConsumerConfig.java
@@ -24,19 +24,19 @@ public class KafkaConsumerConfig {
     @Value("${kafka.groupId}")
     private String groupId;
 
-    private ConsumerFactory<String, byte[]> consumerFactoryString() {
+    private ConsumerFactory<byte[], byte[]> consumerFactoryString() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, url);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, byte[]> kafkaListenerContainerFactoryByte() {
-        ConcurrentKafkaListenerContainerFactory<String, byte[]> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<byte[], byte[]> kafkaListenerContainerFactoryByte() {
+        ConcurrentKafkaListenerContainerFactory<byte[], byte[]> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactoryString());
         return factory;
     }
