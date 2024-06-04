875f877864ae2ed809acd92ca2af9d07613d529d
danila <danilkuzmin2011@gmail.com>
2024-06-04 11:06:40 +0300

    refactor: kafka key ByteArrayDeserializer -> StringDeserializer and add KafkaProperties file

--- src/main/java/danila/mediasoft/test/warehouse/config/KafkaConsumerConfig.java
+++ src/main/java/danila/mediasoft/test/warehouse/config/KafkaConsumerConfig.java
@@ -1,8 +1,9 @@
 package danila.mediasoft.test.warehouse.config;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,25 +18,23 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 @ConditionalOnProperty(prefix = "app", name = "kafka.enabled")
+@RequiredArgsConstructor
 public class KafkaConsumerConfig {
-    @Value("${kafka.url}")
-    private String url;
-    @Value("${kafka.groupId}")
-    private String groupId;
+    private final KafkaProperties kafkaProperties;
 
-    private ConsumerFactory<byte[], byte[]> consumerFactoryString() {
+    private ConsumerFactory<String, byte[]> consumerFactoryString() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, url);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.url());
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.groupId());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<byte[], byte[]> kafkaListenerContainerFactoryByte() {
-        ConcurrentKafkaListenerContainerFactory<byte[], byte[]> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, byte[]> kafkaListenerContainerFactoryByte() {
+        ConcurrentKafkaListenerContainerFactory<String, byte[]> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactoryString());
         return factory;
